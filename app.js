210d43b7d929527bb0ff69055aa20ff0dcecda9d
Warren <warrenroque17@gmail.com>
2024-05-17 10:45:11 -0400

    Set up general html skeleton + div to hold grids

---
+++ app.js
@@ -0,0 +1,3 @@
+// 1. Create webpage with a 16x16 grid of square divs
+// 2. Hover effect over each div that changes colors
+// 3. Add button on top of screen to prompt size of new grid when clicked (max 100)
