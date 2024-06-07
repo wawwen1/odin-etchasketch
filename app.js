851c6ff0815fd37b5eb4c397ed4c94f7dba56a78
Warren <warrenroque17@gmail.com>
2024-06-07 14:18:21 -0400

    Create function to make a grid based off input

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // 1. Create webpage with a 16x16 grid of square divs
 // 2. Hover effect over each div that changes colors
 // 3. Add button on top of screen to prompt size of new grid when clicked (max 100)
+
+const container = document.querySelector("#container");
+
+createGrid = (rows, cols) => {
+  
+  for (let i = 0; i < rows * cols; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid-cell");
+    grid.textContent = "test";
+    container.appendChild(grid);
+
+  }
+};
+
+createGrid(16,16);
